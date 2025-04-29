03a774a65d7c7fd706bcd9b802406570e39b0c96
dulakshaaa <dulaksharajapaksha4@gmail.com>
2025-04-29 16:11:35 +0530

    updating the front end

---
+++ frontend1/src/components/patient/Sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiHome, FiUserPlus, FiUsers, FiSettings } from 'react-icons/fi';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <h2 className="text-center mb-8">Hospital MS</h2>
+      <nav>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <FiHome className="nav-icon" />
+              Dashboard
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/add" className="nav-link">
+              <FiUserPlus className="nav-icon" />
+              Add Patient
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <FiUsers className="nav-icon" />
+              Patients
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a href="#settings" className="nav-link">
+              <FiSettings className="nav-icon" />
+              Settings
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
